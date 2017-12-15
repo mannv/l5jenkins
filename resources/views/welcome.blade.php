5ca0080cc0dab52a5ac381dc65406a2d7e6cea30
man.nv <man.nv@altplus.com.vn>
2017-12-15 14:33:50 +0700

    set env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel ENV: {{config('app.env')}}
                 </div>
 
                 <div class="links">
